aada79d5dbf0b113d632be256f0ca2a51360865b
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-11-20 23:29:43 +0000

    changes

--- eis_triangles.cpp
+++ eis_triangles.cpp
@@ -9,39 +9,48 @@
 #include <string>
 
 int main() {
-    // In this program the letters x, y and z represent the three angle in a triangle.
+    // In this program the letters x, y and z
+    // represent the three angle in a triangle.
+
     // creating variables
-    
-    std::string xString;
-    int xNumber;
-    std::string yString;
-    int yNumber;
-    std::string zString;
-    int zNumber;
+    std::string xAsString;
+    int xAsNumber;
+    std::string yAsString;
+    int yAsNumber;
+    std::string zAsString;
+    int zAsNumber;
 
     // input
     std::cout << "Enter in the 1st angle: ";
-    std::cin >> xNumber;
+    std::cin >> xAsString;
     std::cout << "Enter in the 2nd angle: ";
-    std::cin >> yNumber;
+    std::cin >> yAsString;
     std::cout << "Enter in the 3rd angle: ";
-    std::cin >> zNumber;
+    std::cin >> zAsString;
     std::cout << "\n";
 
     // process and output
     try {
-        xNumber = std::stoi(xString);
-        yNumber = std::stoi(yString);
-        zNumber = std::stoi(zString);
-        if (xNumber == yNumber == zNumber) {
-            std::cout << "The angles" << xString << yString << "and" << zString << "make an Equilateral triangle! " << std::endl;
-        } else if(xNumber == yNumber or yNumber == zNumber or xNumber == zNumber){
-            std::cout << "The angles" << xString << yString << "and" << zString << "make an Isosceles triangle! " << std::endl;
+        xAsNumber = std::stoi(xAsString);
+        yAsNumber = std::stoi(yAsString);
+        zAsNumber = std::stoi(zAsString);
+        if (xAsNumber + yAsNumber + zAsNumber == 180
+            && xAsNumber > 0 && yAsNumber > 0 && zAsNumber > 0) {
+            if (xAsNumber == yAsNumber && yAsNumber == zAsNumber
+                && zAsNumber == xAsNumber) {
+                std::cout <<"These angles make an Equilateral triangle!" << std::endl;
+            } else if (xAsNumber == yAsNumber
+                       || yAsNumber == zAsNumber || xAsNumber == zAsNumber) {
+                std::cout << "These angles make an Isosceles triangle!" << std::endl;
+            } else {
+                std::cout << "These angles make a Scalene triangle!" << std::endl;
+            }
         } else {
-            std::cout << "The angles " << xString << yString << " and " << zString << " make an Scalene triangle!" << std::endl;
+            std::cout << "This is not a triangle" << std::endl;
         }
     } catch (std::invalid_argument) {
         std::cout << "Sorry This is invalid input";
-        std::cout << "\n\nDone.";
     }
+
+    std::cout << "\n\nDone.";
 }
